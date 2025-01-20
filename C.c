36cf9ca65bf910599f26519a6b7ec3e690fcf4dc
Purevbat <purevbat5645@gmail.com>
2025-01-20 13:30:46 +0800

    Tour

--- C.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-int main(){
-    char a[25];
-    get(a, 25, stdin);
-    printf("'%s' has amount of letters %d", a, sizeof(a));
-    return 0;
-}
--- Testpi.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-    int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
-    printf("%d %d %d %d\n", a, b, c, d);
-    return 0;
-}
